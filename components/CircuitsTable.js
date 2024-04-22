37d1090e04a502a57b06fb1422d48cd2a79d2570
AyZedAitch <adam.houlihal@usmba.ac.ma>
2024-04-22 15:35:47 +0100

    pagination, filtering, sorting for circuit table done

--- components/CircuitsTable.js
+++ components/CircuitsTable.js
@@ -1,19 +1,68 @@
 "use client";
 
+import {
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+  getFilteredRowModel,
+} from "@tanstack/react-table";
+import { DateTime } from "luxon";
+import Link from "next/link";
 import React, { useState } from "react";
 
-const CircuitsTable = () => {
+const CircuitsTable = ({ data }) => {
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 5,
+  });
+  const [sorting, setSorting] = useState([]);
+  const [filtering, setFiltering] = useState("");
+  const columns = [
+    {
+      header: "City",
+      accessorKey: "cityName",
+    },
+    {
+      header: "Departure Monument",
+      accessorKey: "departureMonumentName",
+    },
+    {
+      header: "Departure Date",
+      accessorKey: "departureTime",
+      cell: (info) =>
+        DateTime.fromISO(info.getValue()).toLocaleString(DateTime.DATE_MED),
+    },
+    {
+      header: "Action",
+      accessorKey: "circuitId",
+    },
+  ];
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: setPagination,
+    state: { pagination, sorting, globalFilter: filtering },
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setFiltering,
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+  });
+
   return (
     <>
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg ml-5">
-        <div class="pb-4 bg-white dark:bg-gray-900">
-          <label for="table-search" class="sr-only">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg ml-5">
+        <div className="pb-4 bg-white dark:bg-gray-900">
+          <label for="table-search" className="sr-only">
             Search
           </label>
-          <div class="relative mx-3 mt-2">
-            <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+          <div className="relative mx-3 mt-2">
+            <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
               <svg
-                class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -32,128 +81,193 @@ const CircuitsTable = () => {
               type="text"
               id="table-search"
               className="block pt-2 ps-10 text-lg text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search for city"
+              placeholder="Search by keyword"
+              onChange={(event) => setFiltering(event.target.value)}
             />
           </div>
         </div>
 
         <table className="w-full text-lg text-left rtl:text-right text-gray-900 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" class="p-6">
-                City
-              </th>
-              <th scope="col" class="p-6">
-                Departure Monument
-              </th>
-              <th scope="col" class="p-6">
-                Departure Date
-              </th>
-              <th scope="col" class="p-6">
-                Action
-              </th>
-            </tr>
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  if (header.column.id !== "circuitId") {
+                    return (
+                      <th
+                        scope="col"
+                        className="p-6"
+                        key={header.id}
+                        onClick={header.column.getToggleSortingHandler()}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {
+                          { asc: "⬆️", desc: "⬇️" }[
+                            header.column.getIsSorted() ?? null
+                          ]
+                        }
+                      </th>
+                    );
+                  } else {
+                    return (
+                      <th scope="col" className="p-6" key={header.id}>
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      </th>
+                    );
+                  }
+                })}
+              </tr>
+            ))}
           </thead>
           <tbody>
-            {/* <!-- Replace the sample data with your actual data --> */}
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <td class="px-6 py-4">Fes</td>
-              <td class="px-6 py-4">XYZ</td>
-              <td class="px-6 py-4">X/Y/Z date</td>
-
-              <td class="px-6 py-4">
+            {data
+              ? table.getRowModel().rows.map((row) => (
+                  <tr
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                    key={row.id}
+                  >
+                    {row.getVisibleCells().map((cell) => {
+                      if (cell.column.id !== "circuitId") {
+                        return (
+                          <td
+                            className={
+                              cell.column.id === "cityName"
+                                ? "px-6 py-4 uppercase"
+                                : "px-6 py-4"
+                            }
+                            key={cell.id}
+                          >
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                            )}
+                          </td>
+                        );
+                      } else {
+                        return (
+                          <td className="px-6 py-4" key={cell.id}>
+                            <button
+                              className="font-medium text-green-600 dark:text-green-500 hover:underline mr-3"
+                              onClick={() =>
+                                console.log(cell.getContext().getValue())
+                              }
+                            >
+                              View
+                            </button>
+                            <button
+                              className="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-3"
+                              onClick={() =>
+                                console.log(cell.getContext().getValue())
+                              }
+                            >
+                              Edit
+                            </button>
+                            <button
+                              className="font-medium text-red-600 dark:text-red-500 hover:underline mr-3"
+                              onClick={() =>
+                                console.log(cell.getContext().getValue())
+                              }
+                            >
+                              Delete
+                            </button>
+                          </td>
+                        );
+                      }
+                    })}
+                  </tr>
+                ))
+              : null}
+            {/* <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <td className="px-6 py-4">Fes</td>
+              <td className="px-6 py-4">XYZ</td>
+              <td className="px-6 py-4">X/Y/Z date</td>
+              <td className="px-6 py-4">
                 <a
                   href="#"
-                  class="font-medium text-green-600 dark:text-green-500 hover:underline mr-3"
+                  className="font-medium text-green-600 dark:text-green-500 hover:underline mr-3"
                 >
                   View
                 </a>
                 <a
                   href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-3"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-3"
                 >
                   Edit
                 </a>
                 <a
                   href="#"
-                  class="font-medium text-red-600 dark:text-red-500 hover:underline mr-3"
+                  className="font-medium text-red-600 dark:text-red-500 hover:underline mr-3"
                 >
                   Delete
                 </a>
               </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <td class="px-6 py-4">Meknes</td>
-              <td class="px-6 py-4">XYZ</td>
-              <td class="px-6 py-4">X/Y/Z</td>
-
-              <td class="px-6 py-4">
-                <a
-                  href="#"
-                  class="font-medium text-green-600 dark:text-green-500 hover:underline mr-3"
-                >
-                  View
-                </a>
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-3"
-                >
-                  Edit
-                </a>
-                <a
-                  href="#"
-                  class="font-medium text-red-600 dark:text-red-500 hover:underline mr-3"
-                >
-                  Delete
-                </a>
-              </td>
-            </tr>
-            {/* <!-- Add more rows as needed --> */}
+            </tr> */}
           </tbody>
         </table>
-        {/* <!-- Pagination or any other content can be added here --> */}
 
-        <nav
-          class="flex items-center justify-between px-4 py-3 bg-white dark:bg-gray-800 border-t border-gray-200 sm:px-6"
-          aria-label="Pagination"
-        >
-          <div class="hidden sm:block"></div>
-          <div class="flex justify-between sm:justify-end space-x-2">
-            <a
-              href="#"
-              class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white dark:bg-gray-700 dark:text-gray-400 border border-gray-300 dark:border-gray-600 rounded-md hover:bg-gray-50 dark:hover:bg-gray-600"
-            >
-              First
-            </a>
-            <a
-              href="#"
-              class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white dark:bg-gray-700 dark:text-gray-400 border border-gray-300 dark:border-gray-600 rounded-md hover:bg-gray-50 dark:hover:bg-gray-600"
-            >
-              Previous
-            </a>
+        {data ? (
+          <nav
+            className="flex items-center justify-between px-4 py-3 bg-white dark:bg-gray-800 border-t border-gray-200 sm:px-6"
+            aria-label="Pagination"
+          >
+            <div className="hidden sm:block"></div>
+            <div className="flex justify-between sm:justify-end space-x-2">
+              <button
+                className={
+                  "relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md hover:bg-gray-50"
+                }
+                onClick={() => table.setPageIndex(0)}
+              >
+                First
+              </button>
+              <button
+                className={
+                  !table.getCanPreviousPage()
+                    ? "relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 border border-gray-300 rounded-md bg-gray-50"
+                    : "relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md hover:bg-gray-50"
+                }
+                disabled={!table.getCanPreviousPage()}
+                onClick={() => table.previousPage()}
+              >
+                Previous
+              </button>
 
-            {/* <a
+              {/* <a
               href="#"
               aria-current="page"
-              class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-blue-600 bg-blue-100 dark:bg-blue-600 dark:text-white border border-blue-300 dark:border-blue-600 rounded-md"
+              className="relative inline-flex items-center px-4 py-2 text-sm font-medium text-blue-600 bg-blue-100 dark:bg-blue-600 dark:text-white border border-blue-300 dark:border-blue-600 rounded-md"
             >
               3
             </a> */}
 
-            <a
-              href="#"
-              class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white dark:bg-gray-700 dark:text-gray-400 border border-gray-300 dark:border-gray-600 rounded-md hover:bg-gray-50 dark:hover:bg-gray-600"
-            >
-              Next
-            </a>
-            <a
-              href="#"
-              class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white dark:bg-gray-700 dark:text-gray-400 border border-gray-300 dark:border-gray-600 rounded-md hover:bg-gray-50 dark:hover:bg-gray-600"
-            >
-              Last
-            </a>
-          </div>
-        </nav>
+              <button
+                className={
+                  !table.getCanNextPage()
+                    ? "relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 border border-gray-300 rounded-md bg-gray-50"
+                    : "relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md hover:bg-gray-50"
+                }
+                disabled={!table.getCanNextPage()}
+                onClick={() => table.nextPage()}
+              >
+                Next
+              </button>
+              <button
+                className={
+                  "relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md hover:bg-gray-50"
+                }
+                onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+              >
+                Last
+              </button>
+            </div>
+          </nav>
+        ) : null}
       </div>
     </>
   );
